efd9c6f5f4c8b56438938435b6ae5d50995b9326
YoseSandoval <yjsandoval55gmail.com>
2020-10-27 17:17:55 -0600

    Create global.php

---
+++ vistas/global.php
@@ -0,0 +1,21 @@
+<?php 
+//ip de la pc servidor base de datos
+define("DB_HOST", "localhost");
+
+// nombre de la base de datos
+define("DB_NAME", "psps_sg6");
+
+
+//nombre de usuario de base de datos
+define("DB_USERNAME", "psps_sg6");
+
+//conraseña del usuario de base de datos
+define("DB_PASSWORD", "cu9w77XOVf");
+
+//codificacion de caracteres
+define("DB_ENCODE", "utf8");
+
+//nombre del proyecto
+define("PRO_NOMBRE", "DepositoEDM");
+ 
+ ?>
