98249d071b57f993fd3a0a7bd5e6b7103c30dc34
YoseSandoval <yjsandoval55gmail.com>
2020-10-27 17:17:53 -0600

    Create footer.php

---
+++ vistas/footer.php
@@ -0,0 +1,70 @@
+        
+        
+
+
+<!-- jquery
+    ============================================ -->
+    <script src="../public/js/vendor/jquery-1.12.4.min.js"></script>
+    <!-- bootstrap JS
+		============================================ -->
+    <script src="../public/js/bootstrap.min.js"></script>
+    <!-- wow JS
+		============================================ -->
+    <script src="../public/js/wow.min.js"></script>
+    <!-- price-slider JS
+		============================================ -->
+    <script src="../public/js/jquery-price-slider.js"></script>
+    <!-- meanmenu JS
+		============================================ -->
+    <script src="../public/js/jquery.meanmenu.js"></script>
+    <!-- owl.carousel JS
+		============================================ -->
+    <script src="../public/js/owl.carousel.min.js"></script>
+    <!-- sticky JS
+		============================================ -->
+    <script src="../public/js/jquery.sticky.js"></script>
+    <!-- scrollUp JS
+		============================================ -->
+    <script src="../public/js/jquery.scrollUp.min.js"></script>
+    <!-- mCustomScrollbar JS
+		============================================ -->
+    <script src="../public/js/scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script src="../public/js/scrollbar/mCustomScrollbar-active.js"></script>
+    <!-- metisMenu JS
+		============================================ -->
+    <script src="../public/js/metisMenu/metisMenu.min.js"></script>
+    <script src="../public/js/metisMenu/metisMenu-active.js"></script>
+    <!-- sparkline JS
+		============================================ -->
+    <script src="../public/js/sparkline/jquery.sparkline.min.js"></script>
+    <script src="../public/js/sparkline/jquery.charts-sparkline.js"></script>
+    <!-- calendar JS
+		============================================ -->
+    <script src="../public/js/calendar/moment.min.js"></script>
+    <script src="../public/js/calendar/fullcalendar.min.js"></script>
+    <script src="../public/js/calendar/fullcalendar-active.js"></script>
+	<!-- float JS
+		============================================ -->
+    <script src="../public/js/flot/jquery.flot.js"></script>
+    <script src="../public/js/flot/jquery.flot.resize.js"></script>
+    <script src="../public/js/flot/curvedLines.js"></script>
+    <script src="../public/js/flot/flot-active.js"></script>
+    <!-- plugins JS
+		============================================ -->
+    <script src="../public/js/plugins.js"></script>
+    <!-- main JS
+		============================================ -->
+    <script src="../public/js/main.js"></script>
+    <script src="../public/datatables/buttons.colVis.min.js"></script>
+    <script src="../public/datatables/buttons.html5.min.js"></script>
+    <script src="../public/datatables/dataTables.buttons.min.js"></script>
+    <script src="../public/datatables/jquery.dataTables.min.js"></script>
+    <script src="../public/datatables/jszip.min.js"></script>
+
+    <script src="../public/datatables/pdfmake.min.js"></script>
+    <script src="../public/datatables/vfs_fonts.js"></script>
+    <script src="../public/datatables/datatables.min.js"></script>
+    <script src="../public/js/bootbox.min.js"></script>
+    <script src="../public/js/bootstrap-select.min.js"></script>
+</body>
+</html>
